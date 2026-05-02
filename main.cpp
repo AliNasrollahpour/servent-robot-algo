39d0fe223d66765c1ec1f2723a34598c3a4c0684
Ali Nasrollahour <alefittase@Alis-MacBook-Pro.local>
2026-05-02 15:32:11 +0530

    better input handeling

--- main.cpp
+++ main.cpp
@@ -7,29 +7,51 @@ using namespace std;
 struct point{
     int x;
     int y;
+    int type; //0=braier, 1=start, 2=guest, 3=teleport pad, 4=empty
+    int dx; //destenation of teleport
+    int dy; //destenation of teleport
 };
 
-void input(vector<vector<char> >& map, vector<pair<point, point> >& tp, int& initEnergy){
+void input(vector<vector<point> >& map, int& initEnergy){
     int n, m; cin>>n>>m>>initEnergy;
     
-    map.assign(n, vector<char>(m));
+    map.assign(n, vector<point>(m));
     for(int i=0; i<n; i++)
-        for(int j=0; j<0; j++)
-            cin>>map[i][j];
+        for(int j=0; j<0; j++){
+            char tmp; cin>>tmp;
+            point p;
+            p.x=i; p.y=j;
+            switch(tmp){
+                case 'X':
+                    p.type=0;
+                    break;
+                case 'S':
+                    p.type=1;
+                    break;
+                case 'G':
+                    p.type=2;
+                    break;
+                case 'T':
+                    p.type=3;
+                    break;
+                default:
+                    p.type=4;
+            }
+            map[i][j]=p;
+        }
 
     string s;
     while(getline(cin, s)){
-        pair<point, point> t;
-        sscanf(s.c_str(), "Teleport: (%d,%d) -> (%d,%d)", &t.first.x, &t.first.y, &t.second.x, &t.second.y);
-        tp.push_back(t);
+        int x, y, dx, dy;
+        sscanf(s.c_str(), "Teleport: (%d,%d) -> (%d,%d)", &x, &y, &dx, &dy);
+        map[x][y].dx=dx; map[x][y].dy=dy; 
     }
 }
 
 int main(){
-    vector<vector<char> > map;
-    vector<pair<point, point> > tp;
+    vector<vector<point> > map;
     int initEnergy;
-    input(map, tp, initEnergy);
+    input(map, initEnergy);
 
     //--
 }
